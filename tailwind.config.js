b6b55ff2e03e2a770e4cdc56c7bb0913d319eb35
Chad Wright <justdeejays@gmail.com>
2023-06-14 09:46:40 +0200

    feat: init commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    screens: {
+      phone: "425px",
+      tablet: "650px",
+      tabletLarge: "900px",
+      desktopSmall: "1100px",
+      desktop: "1400px",
     },
+    fontWeight: {
+      200: "200",
+      300: "300",
+      350: "350",
+      500: "500",
+      700: "700",
+    },
+    fontSize: {
+      heading: "2rem",
+      subheading: "1.125rem",
+      paragraph: "1rem",
+    },
+    fontFamily: {
+      Open_Sans: ["Open Sans", "sans-serif"],
+    },
+    colors: {
+      blue: "#0C50BE",
+      black: "#1E1E1E",
+      orange: "#F27C27",
+      white: "#FFFFFF",
+      linkBlue: "#3366CC",
+    },
+    extend: {},
   },
   plugins: [],
-}
+};
