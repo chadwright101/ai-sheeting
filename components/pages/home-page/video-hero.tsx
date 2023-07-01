fb2bfa9a00353ca08ef8cdb60279cdd416af17c4
Blyde Wright <blyde.wright@gmail.com>
2023-07-01 14:23:01 +0200

    feat: lazy load video in hero

--- components/pages/home-page/video-hero.tsx
+++ components/pages/home-page/video-hero.tsx
@@ -1,11 +1,29 @@
+import { useEffect, useRef } from "react";
+
 interface Props {
   cssClasses?: string;
 }
 
 const VideoHero = ({ cssClasses }: Props) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  useEffect(() => {
+    const video = videoRef.current;
+
+    if (video) {
+      const sources = video.querySelectorAll("source");
+
+      sources.forEach((source) => {
+        source.setAttribute("src", source.getAttribute("data-src")!);
+      });
+
+      video.load();
+    }
+  }, []);
+
   return (
     <section className={`${cssClasses}`}>
       <video
+        ref={videoRef}
         autoPlay
         muted
         loop
@@ -13,7 +31,14 @@ const VideoHero = ({ cssClasses }: Props) => {
         poster="https://the-wright-designs-website-images.s3.af-south-1.amazonaws.com/ai-sheeting/videos/ai-sheeting-video-poster.webp"
         playsInline
       >
-        <source src="https://the-wright-designs-website-images.s3.af-south-1.amazonaws.com/ai-sheeting/videos/placeholder-video.mp4" />
+        <source
+          type="video/webm"
+          data-src="https://the-wright-designs-website-images.s3.af-south-1.amazonaws.com/ai-sheeting/videos/1400px.webm"
+        />
+        <source
+          type="video/mp4"
+          data-src="https://the-wright-designs-website-images.s3.af-south-1.amazonaws.com/ai-sheeting/videos/1400px.mp4"
+        />
       </video>
     </section>
   );
