feed5abc809fdfac579346294463860020d3feb9
Chad Wright <justdeejays@gmail.com>
2023-07-03 16:09:21 +0200

    fix: add "use client" to testimonials for Splide

--- components/pages/projects-page/testimonials.tsx
+++ components/pages/projects-page/testimonials.tsx
@@ -1,8 +1,10 @@
+"use client";
+
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 
-import SlicedText from "../../utils/sliced-text";
+import SlicedText from "@/components/utils/sliced-text";
 
-import pageData from "../../../data/page-data.json";
+import pageData from "@/data/page-data.json";
 
 import "@splidejs/react-splide/css/core";
 
