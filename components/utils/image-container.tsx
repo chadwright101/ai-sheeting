40ac701e24574bd5eb51748684616d4a83f8312b
Chad Wright <justdeejays@gmail.com>
2024-08-11 19:46:56 +0200

    feat: remove loading state from image-component

--- components/utils/image-container.tsx
+++ components/utils/image-container.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
 
 interface Props {
   src: string;
@@ -40,33 +39,19 @@ const ImageContainer = ({
   onMouseLeave,
   onClick,
 }: Props) => {
-  const [isLoading, setLoading] = useState(true);
-
   return (
     <Image
       alt={alt || "A&I Sheeting"}
       src={src}
       width={width}
       height={height}
-      quality={isLoading ? 5 : quality ?? 60}
+      quality={quality ?? 60}
       loading={eager ? "eager" : "lazy"}
       className={cssClasses}
       onClick={onClick}
-      onLoadStart={() => setLoading(true)}
-      onLoad={() => setLoading(false)}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
-      sizes={`(max-width: 425px) ${
-        isLoading ? smallest! / 10 : smallest
-      }vw,(max-width: 650px) ${
-        isLoading ? phone! / 10 : phone
-      }vw, (max-width: 900px) ${
-        isLoading ? tablet! / 10 : tablet
-      }vw, (max-width: 1100px) ${
-        isLoading ? tabletLarge! / 10 : tabletLarge
-      }vw, (max-width: 1400px) ${
-        isLoading ? desktopSmall! / 10 : desktopSmall
-      }vw, ${isLoading ? desktop! / 10 : desktop}vw `}
+      sizes={`(max-width: 425px) ${smallest}vw,(max-width: 650px) ${phone}vw, (max-width: 900px) ${tablet}vw, (max-width: 1100px) ${tabletLarge}vw, (max-width: 1400px) ${desktopSmall}vw, ${desktop}vw `}
     />
   );
 };
