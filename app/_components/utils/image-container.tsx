d1d0eb74f76ca6b38d8a429680711187b5152aa5
Chad Wright <justdeejays@gmail.com>
2025-12-01 12:33:40 +0200

    fix: update tailwind implementation after upgrading to v4

--- app/_components/utils/image-container.tsx
+++ app/_components/utils/image-container.tsx
@@ -29,26 +29,28 @@ const ImageContainer = ({
   onMouseLeave,
   onClick,
 }: Props) => {
+  const imageQuality = quality ?? 60;
+
   return (
     <picture className={pictureCssClasses}>
       {phoneWidth && (
         <source
           media="(max-width:425px)"
-          srcSet={`https://ik.imagekit.io/thewrightdesigns/a&i-sheeting/tr:w-${phoneWidth},q-70${src}`}
+          srcSet={`https://ik.imagekit.io/thewrightdesigns/a&i-sheeting/tr:w-${phoneWidth},q-${imageQuality}${src}`}
         />
       )}
       {tabletWidth && (
         <source
           media="(max-width:800px)"
-          srcSet={`https://ik.imagekit.io/thewrightdesigns/a&i-sheeting/tr:w-${tabletWidth},q-70${src}`}
+          srcSet={`https://ik.imagekit.io/thewrightdesigns/a&i-sheeting/tr:w-${tabletWidth},q-${imageQuality}${src}`}
         />
       )}
       <Image
         alt={alt || "A&I Sheeting"}
-        src={`https://ik.imagekit.io/thewrightdesigns/a&i-sheeting/tr:w-${desktopWidth},q-70${src}`}
+        src={`https://ik.imagekit.io/thewrightdesigns/a&i-sheeting/tr:w-${desktopWidth},q-${imageQuality}${src}`}
         width={desktopWidth}
         height={desktopWidth * 1.25}
-        quality={quality ?? 60}
+        quality={imageQuality}
         loading={eager ? "eager" : "lazy"}
         className={cssClasses}
         onClick={onClick}
--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   images: {
     unoptimized: true,
+    qualities: [40, 60, 75],
   },
 };
 
